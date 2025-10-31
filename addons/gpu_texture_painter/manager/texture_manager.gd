9c283f6ac7cae492529144813d71f8176be7667f
maantho <dev@antonhoerig.de>
2025-10-31 12:12:19 +0100

    Revise naming convention

--- addons/gpu_texture_painter/manager/texture_manager.gd
+++ addons/gpu_texture_painter/manager/texture_manager.gd
@@ -3,7 +3,7 @@ extends Node
 class_name TextureManager
 
 @export var overlay_material: Material = preload("uid://bdnbf5ol6km4a")
-@export_tool_button("Apply Materials") var tmp = apply_materials
+@export_tool_button("Apply Materials") var tmp = _apply_materials
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,19 +15,19 @@ func _process(delta: float) -> void:
 	pass
 
 
-func apply_materials() -> void:
-	var mesh_instances :=  get_all_mesh_instances(get_parent())
+func _apply_materials() -> void:
+	var mesh_instances :=  _get_all_mesh_instances(get_parent())
 	mesh_instances.pop_front()  # remove parent
 	
 	for mesh_instance in mesh_instances:
 		mesh_instance.material_overlay = overlay_material
 		print("set material")
 
-func get_all_mesh_instances(node: Node, children_acc: Array[MeshInstance3D] = []) -> Array[MeshInstance3D]:
+func _get_all_mesh_instances(node: Node, children_acc: Array[MeshInstance3D] = []) -> Array[MeshInstance3D]:
 	if node is MeshInstance3D:
 		children_acc.push_back(node)
 		
 	for child in node.get_children():
-		children_acc = get_all_mesh_instances(child, children_acc)
+		children_acc = _get_all_mesh_instances(child, children_acc)
 
 	return children_acc

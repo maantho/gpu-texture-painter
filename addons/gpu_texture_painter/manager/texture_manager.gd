cbb9ca32ba4ac2108808520f39040e16e33f6fd1
maantho <dev@antonhoerig.de>
2025-11-02 17:56:33 +0100

    Improve texture creation

--- addons/gpu_texture_painter/manager/texture_manager.gd
+++ addons/gpu_texture_painter/manager/texture_manager.gd
@@ -7,7 +7,7 @@ extends Node
 
 @export var overlay_texture_size: int = 1024
 var overlay_texture_rid: RID = RID()
-var overlay_texture: Texture2DRD = null
+@export_storage var overlay_texture_resource: Texture2DRD = null
 
 var rd: RenderingDevice
 
@@ -25,14 +25,14 @@ func _exit_tree() -> void:
 	if overlay_texture_rid and rd:
 		rd.free_rid(overlay_texture_rid)
 		overlay_texture_rid = RID()
-		overlay_texture.texture_rd_rid = RID()
+		overlay_texture_resource.texture_rd_rid = RID()
 
 func _apply_materials() -> void:
 	var mesh_instances :=  _get_child_mesh_instances(get_parent())
 
 	for mesh_instance in mesh_instances:
 		mesh_instance.material_overlay = overlay_material.duplicate()
-		mesh_instance.material_overlay.set_shader_parameter("overlay_texture", overlay_texture)
+		mesh_instance.material_overlay.set_shader_parameter("overlay_texture", overlay_texture_resource)
 		print("set material")
 
 
@@ -59,14 +59,23 @@ func _create_texture() -> void:
 	fmt.width = overlay_texture_size
 	fmt.height = overlay_texture_size
 	fmt.format = RenderingDevice.DATA_FORMAT_R32G32B32A32_SFLOAT
-	fmt.usage_bits = RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT | RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT
+	fmt.usage_bits = RenderingDevice.TEXTURE_USAGE_SAMPLING_BIT + RenderingDevice.TEXTURE_USAGE_COLOR_ATTACHMENT_BIT + RenderingDevice.TEXTURE_USAGE_STORAGE_BIT + RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT + RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT + RenderingDevice.TEXTURE_USAGE_CAN_COPY_TO_BIT
 
 	# create texture view
 	var view := RDTextureView.new()
 
 	# create texture
-	overlay_texture_rid = rd.texture_create(fmt, view, [])
+	#var image := Image.create(overlay_texture_size, overlay_texture_size, false, Image.FORMAT_RGBAF)
+	var image := Image.load_from_file("uid://b8e56cw41rh6y")
+	overlay_texture_rid = rd.texture_create(fmt, view, [image.get_data()]) 
 
+	_apply_texture_to_texture_resource()
+
+
+func _apply_texture_to_texture_resource() -> void:
 	#create Texture2DRD
-	overlay_texture = overlay_material.get_shader_parameter("overlay_texture")
-	overlay_texture.texture_rd_rid = overlay_texture_rid
+	if not overlay_texture_resource:
+		overlay_texture_resource = Texture2DRD.new()
+	
+	overlay_texture_resource.texture_rd_rid = overlay_texture_rid
+	notify_property_list_changed()
